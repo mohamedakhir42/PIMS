844d583fbdf6254945ff9e243d0173a082a000fa
mohamedakhir42 <mohamedabakhir42@gmail.com>
2026-09-08 16:01:18 +0100

    edit port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 
   server: {
     host: '0.0.0.0',
-    port: Number(process.env.PORT) || 8080,
+    port: Number(process.env.PORT) || 3000,
 
     allowedHosts: [
       'frontend-production-b095.up.railway.app'
